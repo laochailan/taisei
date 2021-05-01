01287bf564b3c1a461be82e43f78071e3078dfec
laochailan <laochailan@web.de>
2021-05-01 21:04:23 +0200

    modernize vlads army except draw rules

--- src/stages/stage4/spells/spells.h
+++ src/stages/stage4/spells/spells.h
@@ -19,6 +19,6 @@ DECLARE_EXTERN_TASK_WITH_INTERFACE(kurumi_redspike, BossAttack);
 DECLARE_EXTERN_TASK_WITH_INTERFACE(kurumi_aniwall, BossAttack);
 DECLARE_EXTERN_TASK_WITH_INTERFACE(kurumi_blowwall, BossAttack);
 DECLARE_EXTERN_TASK_WITH_INTERFACE(kurumi_vampvape, BossAttack);
-void kurumi_extra(Boss*, int);
+DECLARE_EXTERN_TASK_WITH_INTERFACE(kurumi_vladsarmy, BossAttack);
 
 #endif // IGUARD_stages_stage4_spells_spells_h
